e2f112b64cc5c7b06c90374fa338713336f0e3d7
Mohammad Hosein <e.mohamadhosein@gmail.com>
2025-04-22 18:29:02 +0330

    adding contract

---
+++ Lottery/src/Raffle.sol
@@ -0,0 +1,61 @@
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title a simple Raffle contract
+ * @author Mohi
+ * @notice this contract is for creating simple Raffle
+ * @dev Implement Chainlink VRFv2.5
+ */
+contract Raffle {
+    /** Errors */
+    error Raffle__NotEnoughEth();
+
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /** Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if(msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEth();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEntered(msg.sender);
+    }
+
+    function pichWinner() public {
+
+    }
+
+    /** Getter Functions */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
